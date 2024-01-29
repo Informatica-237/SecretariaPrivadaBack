2f6dfab2e171627d949a24969dd117e693afb4dd
Informatica-237 <124802214+Informatica-237@users.noreply.github.com>
2024-01-29 09:56:51 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
 
   // Configurar CORS
   app.enableCors({
-    // origin: 'https://madariagasec.firebaseapp.com',
-    origin: 'http://localhost:8080',
+    origin: 'https://madariagasec.firebaseapp.com',
+    // origin: 'http://localhost:8080',
      // Reemplaza con el origen de tu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
