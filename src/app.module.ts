b62ab1b420d2dbc3b2eaae41156e7f0a868b655d
informatica-237 <pruebasinformatica237@gmail.com>
2024-01-31 12:54:28 -0300

    Update Fl0

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,19 +11,19 @@ import { UsersModule } from './users/users.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
 
-      // BASE DE DATOS ONLINE 
-      // host: 'bjeea2lcgcdmneaujsib-mysql.services.clever-cloud.com',
-      // username: 'uxejyhsch46jhsem',
-      // password: '2maesP71TZv3rr2KcJrc',
-      // database: 'bjeea2lcgcdmneaujsib',
+      // BASE DE DATOS ONLINE
+      host: 'bjeea2lcgcdmneaujsib-mysql.services.clever-cloud.com',
+      username: 'uxejyhsch46jhsem',
+      password: '2maesP71TZv3rr2KcJrc',
+      database: 'bjeea2lcgcdmneaujsib',
 
 
       // BASE DE DATOS LOCAL 
-      host: 'bct3k8v4ehk7fh77hma9-mysql.services.clever-cloud.com', // PRUEBA DB
-      port: 3306,
-      username: 'ualk8hbjhiacgall',
-      password: '7K2Hpj3YyVI0VaHoUKCm',
-      database: 'bct3k8v4ehk7fh77hma9',
+      // host: 'bct3k8v4ehk7fh77hma9-mysql.services.clever-cloud.com', // PRUEBA DB
+      // port: 3306,
+      // username: 'ualk8hbjhiacgall',
+      // password: '7K2Hpj3YyVI0VaHoUKCm',
+      // database: 'bct3k8v4ehk7fh77hma9',
 
 
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
