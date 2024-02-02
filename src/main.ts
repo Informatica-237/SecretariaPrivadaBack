87d96cde8d30e80042291fdfaadcd063e1c57d78
informatica-237 <pruebasinformatica237@gmail.com>
2024-02-02 08:25:56 -0300

    Update Fl0

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,15 @@ async function bootstrap() {
   // Configurar CORS
   app.enableCors({
     origin: 'https://madariagasec.firebaseapp.com',
+
+
+    // origin: 'http://localhost:8080',
+     // Reemplaza con el origen de tu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
  
-  const PORT = process.env.PORT ?? 3000;
+  const PORT = process.env.PORT ?? 3000
 
   await app.listen(PORT);
 }
